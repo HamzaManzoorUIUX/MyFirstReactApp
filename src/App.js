f5f54f18b9f6dcc87add08953cf75557b4ae2ba6
MirzaHamzaManzoor <hamzamanzoor8234@gmail.com>
2018-10-06 17:54:15 +0500

    add imgs and make slider and categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import SideNav from './components/SideNav';
+import Slider from './components/slider';
 const App=()=>{
 return <div>
   <Header/>
   <SideNav/>
+  <Slider/>
+  <h1>
+    <h1>Hamza</h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, voluptas neque? Placeat neque dolore minus nemo, quo dolor atque laborum enim ab distinctio porro ipsam ea, vel quia officia blanditiis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, voluptas neque? Placeat neque dolore minus nemo, quo dolor atque laborum enim ab distinctio porro ipsam ea, vel quia officia blanditiis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, voluptas neque? Placeat neque dolore minus nemo, quo dolor atque laborum enim ab distinctio porro ipsam ea, vel quia officia blanditiis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, voluptas neque? Placeat neque dolore minus nemo, quo dolor atque laborum enim ab distinctio porro ipsam ea, vel quia officia blanditiis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, voluptas neque? Placeat neque dolore minus nemo, quo dolor atque laborum enim ab distinctio porro ipsam ea, vel quia officia blanditiis!
+    </h1>
 </div>
       
 }
